e92ae6c8e045d6ce2f95618b26f5162a795cf23c
Michael Zeolla <62816869+Mikeythunder@users.noreply.github.com>
2021-12-30 16:18:48 -0500

    Init

---
+++ main.js
@@ -0,0 +1,34 @@
+const { Client, Intents } = require('discord.js');
+require('dotenv').config();
+const { playAudio } = require('./playFunctionality');
+
+
+const client = new Client({ intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES] });
+const PREFIX = "!"
+let isReady = false;
+
+
+client.on('ready', () => {
+    isReady = true;
+    console.log(`Ready! - ${client.user.username}`);
+});
+
+
+client.on('messageCreate', message => {
+    console.log(`${message.author.tag}`);
+    if (message.author.bot) return
+    if (message.content.startsWith(PREFIX)) {
+        const args = message.content.slice(PREFIX.length).split(/ +/);
+        console.log(args)
+        const command = args.shift().toLowerCase();
+        console.log(args)
+        if (command === 'play') {
+            playAudio(message, args)
+        }
+    }
+
+});
+
+
+
+client.login(process.env.TOKEN);
